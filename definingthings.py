f9ecc4e0145e8f7639399f5dd18b6751867e2314
TakingCareofBusiness <superspongysquid@yahoo.com>
2023-11-30 14:47:12 -0500

    gang

--- definingthings.py
+++ definingthings.py
@@ -2,20 +2,45 @@ import json
 test = open("data.json", encoding="utf8")
 data = json.load(test)
 class Pokemon():
-    def pokemonteam(self, firstpokemon, firstpokemonmoves, secondpokemon, secondpokemonmoves, thirdpokemon, thirdpokemonmoves, fourthpokemon, fourthpokemonmoves, fifthpokemon, fifthpokemonmoves, sixthpokemon, sixthpokemonmoves):
+    def pokemonteam(self, firstpokemon, firstpokemonmove1, firstpokemonmove2, firstpokemonmove3, firstpokemonmove4, secondpokemon, secondpokemonmove1, secondpokemonmove2, secondpokemonmove3, secondpokemonmove4, thirdpokemon, thirdpokemonmove1, thirdpokemonmove2, thirdpokemonmove3, thirdpokemonmove4, fourthpokemon, fourthpokemonmove1, fourthpokemonmove2, fourthpokemonmove3, fourthpokemonmove4, fifthpokemon, fifthpokemonmove1, fifthpokemonmove2, fifthpokemonmove3, fifthpokemonmove4, sixthpokemon, sixthpokemonmove1, sixthpokemonmove2, sixthpokemonmove3, sixthpokemonmove4):
         self.firstpokemon = firstpokemon
-        self.firstpokemonmoves = firstpokemonmoves
+        self.firstpokemonmove1 = firstpokemonmove1
+        self.firstpokemonmove2 = firstpokemonmove2
+        self.firstpokemonmove3 = firstpokemonmove3
+        self.firstpokemonmove4 = firstpokemonmove4
         self.secondpokemon = secondpokemon
-        self.secondpokemonmoves = secondpokemonmoves
+        self.secondpokemonmove1 = secondpokemonmove1
+        self.secondpokemonmove2 = secondpokemonmove2
+        self.secondpokemonmove3 = secondpokemonmove3
+        self.secondpokemonmove4 = secondpokemonmove4
         self.thirdpokemon = thirdpokemon
-        self.thirdpokemonmoves = thirdpokemonmoves
+        self.thirdpokemonmove1 = thirdpokemonmove1
+        self.thirdpokemonmove2 = thirdpokemonmove2
+        self.thirdpokemonmove3 = thirdpokemonmove3
+        self.thirdpokemonmove4 = thirdpokemonmove4
         self.fourthpokemon = fourthpokemon
-        self.fourthpokemonmoves = fourthpokemonmoves
+        self.fourthpokemonmove1 = fourthpokemonmove1
+        self.fourthpokemonmove2 = fourthpokemonmove2
+        self.fourthpokemonmove3 = fourthpokemonmove3
+        self.fourthpokemonmove4 = fourthpokemonmove4
         self.fifthpokemon = fifthpokemon
-        self.fifthpokemonmoves = fifthpokemonmoves
+        self.fifthpokemonmove1 = fifthpokemonmove1
+        self.fifthpokemonmove2 = fifthpokemonmove2
+        self.fifthpokemonmove3 = fifthpokemonmove3
+        self.fifthpokemonmove4 = fifthpokemonmove4
         self.sixthpokemon = sixthpokemon
-        self.sixthpokemonmoves = sixthpokemonmoves
+        self.sixthpokemonmove1 = sixthpokemonmove1
+        self.sixthpokemonmove2 = sixthpokemonmove2
+        self.sixthpokemonmove3 = sixthpokemonmove3
+        self.sixthpokemonmove4 = sixthpokemonmove4
+        
     pokemonteam()
 class Mike(Pokemon):
-        Pokemon.pokemonteam()
+        def miketeam(self):
+            Pokemon.pokemonteam(self, Charizard, a)
+class Tim(Pokemon):
+     def timteam(self):
+          Pokemon.pokemonteam(self, Pikachu, Surf, Thunderbolt, Submission, Skull Bash, Gyarados, b, Moltress, c, Articuno, d, Venusaur, e, Mewtwo)
+class Simon(Pokemon):
+
         
