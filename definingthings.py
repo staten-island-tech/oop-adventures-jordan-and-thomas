8cd14aa8bf2fb8d47f4c785ceb82fe9555082dc4
TakingCareofBusiness <superspongysquid@yahoo.com>
2023-12-01 14:47:22 -0500

    G

--- definingthings.py
+++ definingthings.py
@@ -2,49 +2,56 @@ import json
 test = open("data.json", encoding="utf8")
 data = json.load(test)
 from teambuilder import Teambuilder
+from teambuilder import playerteam
+from teambuilder import firstpokemonmoves
 class Pokemon(Teambuilder):
-    def pokemonteam(self, firstpokemon, firstpokemonmove1, firstpokemonmove2, firstpokemonmove3, firstpokemonmove4, secondpokemon, secondpokemonmove1, secondpokemonmove2, secondpokemonmove3, secondpokemonmove4, thirdpokemon, thirdpokemonmove1, thirdpokemonmove2, thirdpokemonmove3, thirdpokemonmove4, fourthpokemon, fourthpokemonmove1, fourthpokemonmove2, fourthpokemonmove3, fourthpokemonmove4, fifthpokemon, fifthpokemonmove1, fifthpokemonmove2, fifthpokemonmove3, fifthpokemonmove4, sixthpokemon, sixthpokemonmove1, sixthpokemonmove2, sixthpokemonmove3, sixthpokemonmove4):
-        self.firstpokemon = firstpokemon
-        self.firstpokemonmove1 = firstpokemonmove1
-        self.firstpokemonmove2 = firstpokemonmove2
-        self.firstpokemonmove3 = firstpokemonmove3
-        self.firstpokemonmove4 = firstpokemonmove4
-        self.secondpokemon = secondpokemon
-        self.secondpokemonmove1 = secondpokemonmove1
-        self.secondpokemonmove2 = secondpokemonmove2
-        self.secondpokemonmove3 = secondpokemonmove3
-        self.secondpokemonmove4 = secondpokemonmove4
-        self.thirdpokemon = thirdpokemon
-        self.thirdpokemonmove1 = thirdpokemonmove1
-        self.thirdpokemonmove2 = thirdpokemonmove2
-        self.thirdpokemonmove3 = thirdpokemonmove3
-        self.thirdpokemonmove4 = thirdpokemonmove4
-        self.fourthpokemon = fourthpokemon
-        self.fourthpokemonmove1 = fourthpokemonmove1
-        self.fourthpokemonmove2 = fourthpokemonmove2
-        self.fourthpokemonmove3 = fourthpokemonmove3
-        self.fourthpokemonmove4 = fourthpokemonmove4
-        self.fifthpokemon = fifthpokemon
-        self.fifthpokemonmove1 = fifthpokemonmove1
-        self.fifthpokemonmove2 = fifthpokemonmove2
-        self.fifthpokemonmove3 = fifthpokemonmove3
-        self.fifthpokemonmove4 = fifthpokemonmove4
-        self.sixthpokemon = sixthpokemon
-        self.sixthpokemonmove1 = sixthpokemonmove1
-        self.sixthpokemonmove2 = sixthpokemonmove2
-        self.sixthpokemonmove3 = sixthpokemonmove3
-        self.sixthpokemonmove4 = sixthpokemonmove4
-    pokemonteam()
+    #def pokemonteam(self, firstpokemon, firstpokemonmove1, firstpokemonmove2, firstpokemonmove3, firstpokemonmove4, secondpokemon, secondpokemonmove1, secondpokemonmove2, secondpokemonmove3, secondpokemonmove4, thirdpokemon, thirdpokemonmove1, thirdpokemonmove2, thirdpokemonmove3, thirdpokemonmove4, fourthpokemon, fourthpokemonmove1, fourthpokemonmove2, fourthpokemonmove3, fourthpokemonmove4, fifthpokemon, fifthpokemonmove1, fifthpokemonmove2, fifthpokemonmove3, fifthpokemonmove4, sixthpokemon, sixthpokemonmove1, sixthpokemonmove2, sixthpokemonmove3, sixthpokemonmove4):
+        #self.firstpokemonmove2 = firstpokemonmove2
+        #self.firstpokemonmove3 = firstpokemonmove3
+        #self.firstpokemonmove4 = firstpokemonmove4
+        #self.secondpokemon = secondpokemon
+        #self.secondpokemonmove1 = secondpokemonmove1
+        #self.secondpokemonmove2 = secondpokemonmove2
+        #self.secondpokemonmove3 = secondpokemonmove3
+        #self.secondpokemonmove4 = secondpokemonmove4
+        #self.thirdpokemon = thirdpokemon
+        #self.thirdpokemonmove1 = thirdpokemonmove1
+        #self.thirdpokemonmove2 = thirdpokemonmove2
+        #self.thirdpokemonmove3 = thirdpokemonmove3
+        #self.thirdpokemonmove4 = thirdpokemonmove4
+        #self.fourthpokemon = fourthpokemon
+        #self.fourthpokemonmove1 = fourthpokemonmove1
+        #self.fourthpokemonmove2 = fourthpokemonmove2
+        #self.fourthpokemonmove3 = fourthpokemonmove3
+        #self.fourthpokemonmove4 = fourthpokemonmove4
+        #self.fifthpokemon = fifthpokemon
+        #self.fifthpokemonmove1 = fifthpokemonmove1
+        #self.fifthpokemonmove2 = fifthpokemonmove2
+        #self.fifthpokemonmove3 = fifthpokemonmove3
+        #self.fifthpokemonmove4 = fifthpokemonmove4
+        #self.sixthpokemonmove1 = sixthpokemonmove1
+        #self.sixthpokemonmove2 = sixthpokemonmove2
+        #self.sixthpokemonmove3 = sixthpokemonmove3
+        #self.sixthpokemonmove4 = sixthpokemonmove4
+    #pokemonteam()
+    def spit():
+         print("spit")
+    spit()
     def pokemoninuse():
-        from teambuilder import playerteam
-        from teambuilder import firstpokemonmoves
         Teambuilder.teambuilder
         playerteam.split(",")
         pokemoninuse = playerteam[0]
         choosablemoves = firstpokemonmoves
+        choosablemoves[0] = ("Spit")
         print(choosablemoves)
         choosemove = input("What move would you like to choose? ")
         if choosemove == choosablemoves[0]:
+             Pokemon.choosemove
+    pokemoninuse
+    
+             
+             
+             
             
         
     pokemoninuse()
