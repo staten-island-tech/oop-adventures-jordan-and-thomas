57fc353c0ea467c8f71d46395818cfdf0a13e9c6
JordanC09 <144480327+JordanC09@users.noreply.github.com>
2023-12-07 12:16:20 -0500

    testing uno dos thres

--- test.py
+++ test.py
@@ -1,20 +1,34 @@
 import json
 
 import random
-with open("data.json", "r") as f:
+#with open("data.json", "r") as f:
     # Serialize the updated Python list to a JSON string
-    data = json.load(f)
+    #data = json.load(f)
 
-b = len(data)
+#b = len(data)
 
 
-x = (random.randrange(10))
-print(x)
-if x == 1 or x == 2 or x == 3:
-    for i in range(b):
-        if data[i]["type"] == "Bug":
-            print(data[i]["name"])
-if x == 4 or x == 5 or x == 6 or x == 7:
-    for i in range(b):
-        if data[i]["type"] == "Normal":
-            print(data[i]["name"])
+#x = (random.randrange(10))
+#print(x)
+#if x == 1 or x == 2 or x == 3:
+#    for i in range(b):
+ #       if data[i]["type"] == "Bug":
+  #          print(data[i]["name"])
+#if x == 4 or x == 5 or x == 6 or x == 7:
+ #   for i in range(b):
+  #      if data[i]["type"] == "Normal":
+#   print(data[i]["name"])
+
+
+x = int(input("times: "))
+number = 1
+def testing():
+    for i in range(x):
+        number =+ 1
+
+def running(number):
+    for i in range(number):
+        print("hi")
+
+testing()
+running(number)
