436352d80d796859a4475d62863310326e491363
TakingCareofBusiness <superspongysquid@yahoo.com>
2023-12-12 14:07:16 -0500

    a

--- tryingtofix.py
+++ tryingtofix.py
@@ -3,18 +3,31 @@ test = open("data.json", encoding="utf8")
 data = json.load(test)
 playerteam = []
 pokemonmoves = []
+global pokemonnumber
 global p
-p = True
 def pickpokemon():
+    p = True
     while p == True:
         pokemon = input("Type pokemon here ")
         for i in range(len(data)):
             if pokemon == (data[i]["Name"]):
                 playerteam.append(pokemon)
+                pokemonnumber = i
                 p = False
+pickpokemon()
 global m
-m = True
 def pickmove():
+    m = True
     while m == True:
-        moves = input("")
+        move = input("Type moves here")
+        for i in range(len(data)[pokemonnumber]["Learnable Moves"]):
+            if move == (data[pokemonnumber]["Learnable Moves"]):
+                pokemonmoves.append(move)
+                m = False
+pickmove()
+print(playerteam)
+print(pokemonmoves)
+
+
+
 
