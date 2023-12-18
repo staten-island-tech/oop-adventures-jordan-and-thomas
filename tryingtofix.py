4ec4d24a16256d19c343af0ae9b3e5331f9113ac
TakingCareofBusiness <superspongysquid@yahoo.com>
2023-12-18 14:06:43 -0500

    gang

--- tryingtofix.py
+++ tryingtofix.py
@@ -8,45 +8,63 @@ allpokemon = []
 for i in range(len(data)):
             allpokemon.append(data[i]["Name"])
 global p
+global pcheck
 global m
+global mcheck
 global m2
+global m2check
 global m3
+global m3check
 global m4
+global m4check
 class Teambuilder():
-    def pickpokemon(playerpokemon):
+    def pickpokemon():
         p = True
+        pcheck = True
         while p == True:
-            for i in range(len(data)):
-                if playerpokemon == data[i]["Name"]:
-                    playerteam.append(playerpokemon)
-                    pokemonnumber = i
-                    pknumber[0] = (pokemonnumber)
-                    p = False
-    def pickmove(move):
+                playerpokemon = input("Type pokemon here ")
+                for i in range(len(data)):
+                    if playerpokemon == data[i]["Name"]:
+                        if len(playerteam) == 0:
+                            playerteam.append(playerpokemon)
+                            pokemonnumber = i
+                            pknumber[0] = (pokemonnumber)
+                            p = False
+                        if len(playerteam) != 0 or len(playerteam) != 1:
+                            for i in range(len(playerteam)):
+                                playerteam.append(playerpokemon)
+                                pokemonnumber = i
+                                pknumber[0] = pokemonnumber
+                                if playerpokemon == playerteam[i]:
+                                    pcheck = False
+                            if pcheck == True:
+                                p = False
+    def pickmove():
         currentpkmoves = []
         m = True
         while m == True:
+            move = input("Type move here ")
             for i in range(len(data[pknumber[0]]["Learnable Moves"])):
                 if move == data[pknumber[0]]["Learnable Moves"][i]:
                     currentpkmoves.append(move)
                     m = False
         m2 = True
-        move2 = input("Type move here ")
         while m2 == True:
+            move2 = input("Type move here ")
             for i in range(len(data[pknumber[0]]["Learnable Moves"])):
                 if move2 == data[pknumber[0]]["Learnable Moves"][i]:
                     currentpkmoves.append(move2)
                     m2 = False
         m3 = True
-        move3 = input("Type move here ")
         while m3 == True:
+            move3 = input("Type move here ")
             for i in range(len(data[pknumber[0]]["Learnable Moves"])):
                 if move3 == data[pknumber[0]]["Learnable Moves"][i]:
                     currentpkmoves.append(move3)
                     m3 = False
-        m4= True
-        move4 = input("Type move here ")
+        m4 = True
         while m4 == True:
+            move4 = input("Type move here ")
             for i in range(len(data[pknumber[0]]["Learnable Moves"])):
                 if move4 == data[pknumber[0]]["Learnable Moves"][i]:
                     currentpkmoves.append(move4)
@@ -55,11 +73,9 @@ class Teambuilder():
     def teambuilder(self):
         for i in range(5):
             print(allpokemon)
-            inputpokemon = input("Type pokemon here ")
-            Teambuilder.pickpokemon(inputpokemon)
+            Teambuilder.pickpokemon()
             print(data[pknumber[0]]["Learnable Moves"])
-            inputmove = input("Type move here ")
-            Teambuilder.pickmove(inputmove)
+            Teambuilder.pickmove()
 
 tb = Teambuilder()
 tb.teambuilder()
