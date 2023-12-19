861c392a4bbe939556e704c4541d517c1b73baeb
TakingCareofBusiness <superspongysquid@yahoo.com>
2023-12-19 14:02:02 -0500

    gang

--- test.py
+++
@@ -1,6 +0,0 @@
-firstpokemon = input("Try again ")
-        for i in range(len(data)):
-            if firstpokemon == (data[i]["Name"]):
-                playerteam.append(firstpokemon)
-                break
-            
