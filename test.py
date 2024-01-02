f0b1d057878b55ef311141758f36be8032c5ecab
JordanC09 <144480327+JordanC09@users.noreply.github.com>
2024-01-02 13:19:26 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,1 @@
-firstpokemon = input("Try again ")
-        for i in range(len(data)):
-            if firstpokemon == (data[i]["Name"]):
-                playerteam.append(firstpokemon)
-                break
-            
+
