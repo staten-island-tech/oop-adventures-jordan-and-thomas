f2e7becfe1d1813babd8f5720b36678825ceda3d
TakingCareofBusiness <superspongysquid@yahoo.com>
2024-01-15 19:47:48 -0500

    working hard

--- turns.py
+++ turns.py
@@ -668,7 +668,383 @@ class functionality():
         
 
     
+def typechart(enemypokemon, oppositepokemon):
+        global matchup
+        global matchup1
+        Doit = "yes"
+        matchup = "normal"
+        matchup1 = "normal"
+        for i in range(pokemonlist):
+            if (data[i]["Name"]) == oppositepokemon:
+                ptype = (data[i]["Types"])
+        for i in range(pokemonlist):
+            if (data[i]["Name"]) == enemypokemon:
+                enemytypes = (data[i]["Types"])
+        enemytype = enemytypes[0]
+        if len(enemytypes) == 1:
+          Doit = "no"
+          
+        if len(enemytypes) == 2:
+          enemytype2 = enemytypes[1]
+        #if len(types) == 2: 2 variables
 
+        if enemytype == "Normal" and "Rock" in ptype:
+            matchup = "half"
+        if enemytype == "Normal" and "Ghost" in ptype:
+            matchup = "zero"
+
+        if enemytype == "Fire" and "Fire" in ptype:
+            matchup = "half"
+        if enemytype == "Fire" and "Water" in ptype:
+            matchup = "half"
+        if enemytype == "Fire" and "Grass" in ptype:
+            matchup = "super"
+        if enemytype == "Fire" and "Ice" in ptype:
+            matchup = "super"
+        if enemytype == "Fire" and "Bug" in ptype:
+            matchup = "super"
+        if enemytype == "Fire" and "Rock" in ptype:
+            matchup = "half"
+        if enemytype == "Fire" and "Dragon" in ptype:
+            matchup = "half"
+
+        if enemytype == "Water" and "Fire" in ptype:
+            matchup = "super"
+        if enemytype == "Water" and "Water" in ptype:
+            matchup = "half"
+        if enemytype == "Water" and "Grass" in ptype:
+            matchup = "half"
+        if enemytype == "Water" and "Ground" in ptype:
+            matchup = "super"
+        if enemytype == "Water" and "Rock" in ptype:
+            matchup = "super"
+        if enemytype == "Water" and "Dragon" in ptype:
+            matchup = "half"
+
+        if enemytype == "Electric" and "Water" in ptype:
+            matchup = "super"
+        if enemytype == "Electic" and "Electirc" in ptype:
+            matchup = "half"
+        if enemytype == "Electric" and "Grass" in ptype:
+            matchup = "half"
+        if enemytype == "Electric" and "Ground" in ptype:
+            matchup = "zero"
+        if enemytype == "Electric" and "Flying" in ptype:
+            matchup = "super"
+        if enemytype == "Electric" and "Dragon" in ptype:
+            matchup = "half"
+
+        if enemytype == "Grass" and "Fire" in ptype:
+            matchup = "half"
+        if enemytype == "Grass" and "Water" in ptype:
+            matchup = "super"
+        if enemytype == "Grass" and "Grass" in ptype:
+            matchup = "half"
+        if enemytype == "Grass" and "Poison" in ptype:
+            matchup = "half"
+        if enemytype == "Grass" and "Ground" in ptype:
+            matchup = "super"
+        if enemytype == "Grass" and "Flying" in ptype:
+            matchup = "half"
+        if enemytype == "Grass" and "Bug" in ptype:
+            matchup = "half"
+        if enemytype == "Grass" and "Rock" in ptype:
+            matchup = "super"
+        if enemytype == "Grass" and "Dragon" in ptype:
+            matchup = "half"
+
+        if enemytype == "Ice" and "Fire" in ptype:
+            matchup = "half"
+        if enemytype == "Ice" and "Water" in ptype:
+            matchup = "half"
+        if enemytype == "Ice" and "Grass" in ptype:
+            matchup = "super"
+        if enemytype == "Ice" and "Ice" in ptype:
+            matchup = "half"
+        if enemytype == "Ice" and "Ground" in ptype:
+            matchup = "super"
+        if enemytype == "Ice" and "Flying" in ptype:
+            matchup = "super"
+        if enemytype == "Ice" and "Dragon" in ptype:
+            matchup = "super"
+
+        if enemytype == "Fighting" and "Normal" in ptype:
+            matchup = "super"
+        if enemytype == "Fighting" and "Ice" in ptype:
+            matchup = "super"
+        if enemytype == "Fighting" and "Poison" in ptype:
+            matchup = "half"
+        if enemytype == "Fighting" and "Flying" in ptype:
+            matchup = "half"
+        if enemytype == "Fighting" and "Psychic" in ptype:
+            matchup = "half"
+        if enemytype == "Fighting" and "Bug" in ptype:
+            matchup = "half"
+        if enemytype == "Fighting" and "Rock" in ptype:
+            matchup = "super"
+        if enemytype == "Fighting" and "Ghost" in ptype:
+            matchup = "zero"
+
+        if enemytype == "Poison" and "Grass" in ptype:
+            matchup = "super"
+        if enemytype == "Poison" and "Poison" in ptype:
+            matchup = "half"
+        if enemytype == "Poison" and "Ground" in ptype:
+            matchup = "half"
+        if enemytype == "Poison" and "Rock" in ptype:
+            matchup = "half"
+        if enemytype == "Poison" and "Ghost" in ptype:
+            matchup = "half"
+
+        if enemytype == "Ground" and "Fire" in ptype:
+            matchup = "super"
+        if enemytype == "Ground" and "Electric" in ptype:
+            matchup = "super"
+        if enemytype == "Ground" and "Grass" in ptype:
+            matchup = "half"
+        if enemytype == "Ground" and "Poison" in ptype:
+            matchup == "super"
+        if enemytype == "Ground" and "Flying" in ptype:
+            matchup == "zero"
+        if enemytype == "Ground" and "Bug" in ptype:
+            matchup == "half"
+        if enemytype == "Ground" and "Rock" in ptype:
+            matchup = "super"
+
+        if enemytype == "Flying" and "Electric" in ptype:
+            matchup = "half"
+        if enemytype == "Flying" and "Grass" in ptype:
+            matchup = "super"
+        if enemytype == "Flying" and "Fighting" in ptype:
+            matchup = "super"
+        if enemytype == "Flying" and "Bug" in ptype:
+            matchup = "Bug"
+        if enemytype == "Flying" and "Rock" in ptype:
+            matchup = "half"
+
+        if enemytype == "Psychic" and "Fighting" in ptype:
+            matchup = "super"
+        if enemytype == "Psychic" and "Poison" in ptype:
+            matchup = "super"
+        if enemytype == "Psychic" and "Psychic" in ptype:
+            matchup = "half"
+
+        if enemytype == "Bug" and "Fire" in ptype:
+            matchup = "half"
+        if enemytype == "Bug" and "Grass" in ptype:
+            matchup = "super"
+        if enemytype == "Bug" and "Fighting" in ptype:
+            matchup = "half"
+        if enemytype == "Bug" and "Poison" in ptype:
+            matchup = "half"
+        if enemytype == "Bug" and "Flying" in ptype:
+            matchup = "half"
+        if enemytype == "Bug" and "Psychic" in ptype:
+            matchup = "super"
+        if enemytype == "Bug" and "Ghost" in ptype:
+            matchup = "half"
+
+        if enemytype == "Rock" and "Fire" in ptype:
+            matchup = "super"
+        if enemytype == "Rock" and "Ice" in ptype:
+            matchup = "super"
+        if enemytype == "Rock" and "Fighting" in ptype:
+            matchup = "half"
+        if enemytype == "Rock" and "Ground" in ptype:
+            matchup = "half"
+        if enemytype == "Rock" and "Flying" in ptype:
+            matchup = "super"
+        if enemytype == "Rock" and "Bug" in ptype:
+            matchup = "super"
+
+        if enemytype == "Ghost" and "Normal" in ptype:
+            matchup = "zero"
+        if enemytype == "Ghost" and "Psychic" in ptype:
+            matchup = "super"
+        if enemytype == "Ghost" and "Ghost" in ptype:
+            matchup = "super"
+
+        if enemytype == "Dragon" and "Dragon" in ptype:
+            matchup = "super"
+        if Doit == "yes":
+          if enemytype2 == "Normal" and "Rock" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Normal" and "Ghost" in ptype:
+            matchup1 = "zero"
+
+          if enemytype2 == "Fire" and "Fire" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Fire" and "Water" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Fire" and "Grass" in ptype:
+            matchup1 = "super"
+          if enemytype2 == "Fire" and "Ice" in ptype:
+            matchup1 = "super"
+          if enemytype2 == "Fire" and "Bug" in ptype:
+            matchup1 = "super"
+          if enemytype2 == "Fire" and "Rock" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Fire" and "Dragon" in ptype:
+            matchup1 = "half"
+
+          if enemytype2 == "Water" and "Fire" in ptype:
+            matchup1 = "super"
+          if enemytype2 == "Water" and "Water" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Water" and "Grass" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Water" and "Ground" in ptype:
+            matchup1 = "super"
+          if enemytype2 == "Water" and "Rock" in ptype:
+            matchup1 = "super"
+          if enemytype2 == "Water" and "Dragon" in ptype:
+            matchup1 = "half"
+
+          if enemytype2 == "Electric" and "Water" in ptype:
+            matchup1 = "super"
+          if enemytype2 == "Electic" and "Electirc" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Electric" and "Grass" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Electric" and "Ground" in ptype:
+            matchup1 = "zero"
+          if enemytype2 == "Electric" and "Flying" in ptype:
+            matchup1 = "super"
+          if enemytype2 == "Electric" and "Dragon" in ptype:
+            matchup1 = "half"
+
+          if enemytype2 == "Grass" and "Fire" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Grass" and "Water" in ptype:
+            matchup1 = "super"
+          if enemytype2 == "Grass" and "Grass" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Grass" and "Poison" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Grass" and "Ground" in ptype:
+            matchup1 = "super"
+          if enemytype2 == "Grass" and "Flying" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Grass" and "Bug" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Grass" and "Rock" in ptype:
+            matchup1 = "super"
+          if enemytype2 == "Grass" and "Dragon" in ptype:
+            matchup1 = "half"
+
+          if enemytype2 == "Ice" and "Fire" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Ice" and "Water" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Ice" and "Grass" in ptype:
+            matchup1 = "super"
+          if enemytype2 == "Ice" and "Ice" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Ice" and "Ground" in ptype:
+            matchup1 = "super"
+          if enemytype2 == "Ice" and "Flying" in ptype:
+            matchup1 = "super"
+          if enemytype2 == "Ice" and "Dragon" in ptype:
+            matchup1 = "super"
+
+          if enemytype2 == "Fighting" and "Normal" in ptype:
+            matchup1 = "super"
+          if enemytype2 == "Fighting" and "Ice" in ptype:
+            matchup1 = "super"
+          if enemytype2 == "Fighting" and "Poison" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Fighting" and "Flying" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Fighting" and "Psychic" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Fighting" and "Bug" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Fighting" and "Rock" in ptype:
+            matchup1 = "super"
+          if enemytype2 == "Fighting" and "Ghost" in ptype:
+            matchup1 = "zero"
+
+          if enemytype2 == "Poison" and "Grass" in ptype:
+            matchup1 = "super"
+          if enemytype2 == "Poison" and "Poison" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Poison" and "Ground" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Poison" and "Rock" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Poison" and "Ghost" in ptype:
+            matchup1 = "half"
+
+          if enemytype2 == "Ground" and "Fire" in ptype:
+            matchup1 = "super"
+          if enemytype2 == "Ground" and "Electric" in ptype:
+            matchup1 = "super"
+          if enemytype2 == "Ground" and "Grass" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Ground" and "Poison" in ptype:
+            matchup1 == "super"
+          if enemytype2 == "Ground" and "Flying" in ptype:
+            matchup1 == "zero"
+          if enemytype2 == "Ground" and "Bug" in ptype:
+            matchup1 == "half"
+          if enemytype2 == "Ground" and "Rock" in ptype:
+            matchup1 = "super"
+
+          if enemytype2 == "Flying" and "Electric" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Flying" and "Grass" in ptype:
+            matchup1 = "super"
+          if enemytype2 == "Flying" and "Fighting" in ptype:
+            matchup1 = "super"
+          if enemytype2 == "Flying" and "Bug" in ptype:
+            matchup1 = "Bug"
+          if enemytype2 == "Flying" and "Rock" in ptype:
+            matchup1 = "half"
+
+          if enemytype2 == "Psychic" and "Fighting" in ptype:
+            matchup1 = "super"
+          if enemytype2 == "Psychic" and "Poison" in ptype:
+            matchup1 = "super"
+          if enemytype2 == "Psychic" and "Psychic" in ptype:
+            matchup1 = "half"
+
+          if enemytype2 == "Bug" and "Fire" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Bug" and "Grass" in ptype:
+            matchup1 = "super"
+          if enemytype2 == "Bug" and "Fighting" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Bug" and "Poison" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Bug" and "Flying" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Bug" and "Psychic" in ptype:
+            matchup1 = "super"
+          if enemytype2 == "Bug" and "Ghost" in ptype:
+            matchup1 = "half"
+
+          if enemytype2 == "Rock" and "Fire" in ptype:
+            matchup1 = "super"
+          if enemytype2 == "Rock" and "Ice" in ptype:
+            matchup1 = "super"
+          if enemytype2 == "Rock" and "Fighting" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Rock" and "Ground" in ptype:
+            matchup1 = "half"
+          if enemytype2 == "Rock" and "Flying" in ptype:
+            matchup1 = "super"
+          if enemytype2 == "Rock" and "Bug" in ptype:
+            matchup1 = "super"
+
+          if enemytype2 == "Ghost" and "Normal" in ptype:
+            matchup1 = "zero"
+          if enemytype2 == "Ghost" and "Psychic" in ptype:
+            matchup1 = "super"
+          if enemytype2 == "Ghost" and "Ghost" in ptype:
+            matchup1 = "super"
+
+          if enemytype2 == "Dragon" and "Dragon" in ptype:
+            matchup1 = "super"
     def checks(enemypokemo, oppositepokemon):
         global enemypokemon
         global enemyspeed
